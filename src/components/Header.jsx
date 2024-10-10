941fcbfe3e4d3c3984f4ede1a9238504940a7f8d
LeeChengZhan2 <leechengzhan7@gmail.com>
2024-10-10 18:07:53 +0800

    Installed Tailwind CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,30 @@ import React from "react";
 
 function Header(){
     return(
-    <div>
-        <p>This is a Header</p>
-    </div>
+        <header className="bg-gray-800 text-white">
+        <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+          <div className="text-2xl font-bold">
+            <a href="/">Logo</a>
+          </div>
+          <nav>
+            <ul className="flex space-x-4">
+              <li>
+                <a href="/" className="hover:text-gray-400">Home</a>
+              </li>
+              <li>
+                <a href="/about" className="hover:text-gray-400">About</a>
+              </li>
+              <li>
+                <a href="/services" className="hover:text-gray-400">Services</a>
+              </li>
+              <li>
+                <a href="/contact" className="hover:text-gray-400">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
     );
-}
-
-export default Header;
+  };
+  
+  export default Header;
