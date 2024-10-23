45007e36cd262d2a4f5b3342a1e23375650afcb9
LeeChengZhan2 <leechengzhan7@gmail.com>
2024-10-23 17:50:46 +0800

    Update Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,12 @@ import React from "react";
 function Header(){
     return(
         <header className="bg-gray-800 text-white">
-        <div className="container mx-auto px-10 py-2 flex justify-between items-center">
+        <div className="container mx-auto px-10 py-3 flex justify-between items-center">
           <div className="text-2xl font-bold">
             <a href="/">Logo</a>
           </div>
           <nav>
-            <ul className="flex space-x-6">
+            <ul className="flex space-x-7">
               <li>
                 <a href="/" className="nav-link">Home</a>
               </li>
