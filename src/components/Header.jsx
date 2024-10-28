762aec9559c8de09331c98ddd89b7ab99a516998
LeeChengZhan2 <leechengzhan7@gmail.com>
2024-10-28 17:10:34 +0800

    Update Header and add Home Page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,29 +2,14 @@ import React from "react";
 
 function Header(){
     return(
-        <header className="bg-gray-800 text-white">
-        <div className="container mx-auto px-10 py-3 flex justify-between items-center">
-          <div className="text-2xl font-bold">
-            <a href="/">Logo</a>
-          </div>
-          <nav>
-            <ul className="flex space-x-7">
-              <li>
-                <a href="/" className="nav-link">Home</a>
-              </li>
-              <li>
-                <a href="/about" className="nav-link">About</a>
-              </li>
-              <li>
-                <a href="/services" className="nav-link">Portfolio</a>
-              </li>
-              <li>
-                <a href="/contact" className="nav-link">Contact</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
+      <header className="bg-white p-4 shadow-md">
+      <nav className="flex justify-center space-x-8">
+        <a href="#home" className="nav-link">Home</a>
+        <a href="#about" className="nav-link">About</a>
+        <a href="#portfolio" className="nav-link">Portfolio</a>
+        <a href="#contact" className="nav-link">Contact</a>
+      </nav>
+    </header>
     );
   };
   
