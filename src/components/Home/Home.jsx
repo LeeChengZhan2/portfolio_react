a5de5a38a2b5cd975a9ffbab59f84c5c07f867a7
LeeChengZhan2 <leechengzhan7@gmail.com>
2024-11-05 18:02:06 +0800

    Update responsive size and Home intro aligment

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,8 +6,9 @@ function Home(){
         <main className="main-container">
             <section className="content-container">
                 <section className="text-container">
-                    <h1 className="introduction">Hi, I'm Lee Cheng Zhan from Malaysia</h1>
+                    <p className="personalities">INFJ</p>
                     <p className="occupation">Software Engineer</p>
+                    <h1 className="introduction">Hi, I'm Lee Cheng Zhan from Malaysia</h1>
                 </section>
             </section>
         </main>
